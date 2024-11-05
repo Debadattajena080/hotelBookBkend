16f3248219ae767060e27f358794c65284c1b72f
DEBADATTAJENA <debadattajena25@gmail.com>
2024-11-05 17:21:11 +0530

    Small changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import multer from "multer";
 import Hotel from "./models/Hotel.Model.js";
